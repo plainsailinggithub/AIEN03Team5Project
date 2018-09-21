943fc4301307967d7f2dde752519106749a53364
Joey <joey@iii.org.tw>
2018-09-21 11:36:36 +0800

    member app

--- team5project/urls.py
+++ team5project/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',include('member.urls')),
     path('article', include('article.urls')),
 ]
