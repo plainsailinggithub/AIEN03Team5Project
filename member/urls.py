ee453699411613ba74ab79f42ce5c062d5d28c45
Joey <joey@iii.org.tw>
2018-09-21 11:41:33 +0800

    login & member

---
+++ member/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name='member'
+urlpatterns = [
+    path('',views.index,name='index'),
+    path('member/',views.member,name='member'),
+
+
+]
--- member/views.py
+++ member/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def index(request):
+    
+    
+    return render(request,'home/index.html', locals())
+
+def member(request):
+    return render(request,'home/member.html', locals())
