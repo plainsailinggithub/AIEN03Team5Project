16420cb9a42378afc008b4bc541fdfab9da0e648
RyanLin44 <plainsailingknowledge@gmail.com>
2018-10-03 14:05:48 +0800

    dddd

--- index/urls.py
+++ index/urls.py
@@ -7,7 +7,7 @@ app_name = "index"
 
 urlpatterns = [
     #http://localhost:8000/index/
-    path('',views.index,name="index1"),  #urlName
+    path('',views.index,name="index11111"),  #urlName
     #views表示views.py的module
     #index表示views.py下的function
     
