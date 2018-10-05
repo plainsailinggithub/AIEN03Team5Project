3a39545dbf9793ce478c0be7e634e1f6ce052da1
Joey <joey@iii.org.tw>
2018-10-05 17:36:20 +0800

    個人資料可以修改了+主板的header有加了一小段東西

--- member/urls.py
+++ member/urls.py
@@ -7,7 +7,8 @@ urlpatterns = [
     path('login/',views.login,name='login'),
     path('register/',views.register,name='register'),
     path('member/',views.member,name='member'),
-    path('setting/',views.setting,name='setting'),
+    # path('setting/',views.setting,name='setting'),
+    path('setting/<int:id>', views.setting, name="setting"),
     path('logout/',views.logout,name="logout"),
     path('check/<str:emailid>/', views.checkname, name="checkname"),
 
