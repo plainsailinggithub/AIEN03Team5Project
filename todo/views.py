4900423e23f948d2e89420273cc9cb1f964516fa
Liu An-Hua <anglejaina@msn.com>
2018-10-08 17:36:04 +0800

    訊息存資料庫 對話基本沒問題

--- todo/views.py
+++ todo/views.py
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
-from todo.models import Todo,Member,Friendship,Msg
-from todo.serializers import TodoSerializre,MemberSerializre,FriendshipSerializre,MsgSerializre
-=======
-from todo.models import Todo,Member,Friendship,Message,Articles
-from todo.serializers import TodoSerializre,MemberSerializre,FriendshipSerializre,MessageSerializre,ArticleSerializer
->>>>>>> b60b961dae231a3e801fa02d90ce4c3bbf9937a5
+from todo.models import Todo,Member,Friendship,Msg,Articles
+from todo.serializers import TodoSerializre,MemberSerializre,FriendshipSerializre,MsgSerializre,ArticleSerializer
 from rest_framework import viewsets
 # Create your views here.
 
@@ -20,16 +15,10 @@ class FriendshipViewSet(viewsets.ModelViewSet):
     queryset = Friendship.objects.all()
     serializer_class = FriendshipSerializre 
 
-<<<<<<< HEAD
 class MsgViewSet(viewsets.ModelViewSet):
     queryset = Msg.objects.all()
     serializer_class = MsgSerializre         
-=======
-class MessageViewSet(viewsets.ModelViewSet):
-    queryset = Message.objects.all()
-    serializer_class = MessageSerializre      
 
 class ArticlesViewset(viewsets.ModelViewSet):
     queryset = Articles.objects.all()
     serializer_class = ArticleSerializer
->>>>>>> b60b961dae231a3e801fa02d90ce4c3bbf9937a5
