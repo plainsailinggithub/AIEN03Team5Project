c199a9d11cb1d4a0d415db23ff8437a5c9b9e3ab
Liu An-Hua <anglejaina@msn.com>
2018-10-09 09:11:09 +0800

    同意

--- member/models.py
+++ member/models.py
@@ -21,5 +21,5 @@ class Members(models.Model):
     language = models.CharField(max_length=45, blank=True, null=True)
 
     class Meta:
-        managed = True
+        managed = False
         db_table = 'members'
