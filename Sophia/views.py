dc96fce1a39e291bd6098985e2e6743141fb0f90
Sophia <cma@iii.org.tw>
2018-10-11 18:06:45 +0800

    news

--- Sophia/views.py
+++ Sophia/views.py
@@ -7,16 +7,11 @@ from bs4 import BeautifulSoup
 import re
 
 
-
 # Create your views here.
 def index(request):
     Economist.objects.all().delete()
     scraping()
     return render(request, 'Sophia/index.html', locals())
-
-
-
-
 
 def scraping():
     url = "https://www.cw.com.tw/search/doSearch.action?getSort=&channel=ch16&key=%E8%96%AA%E6%B0%B4"
@@ -25,11 +20,9 @@ def scraping():
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'
     }
 
-
     response = requests.get(url, headers=headers_data)
     response.encoding = "utf-8"
     soup = BeautifulSoup(response.text, "lxml")
-
 
     a = soup.select("div.articleGroup section h3 a")
     for url in a:
@@ -42,7 +35,6 @@ def scraping():
         "url":c,
         }
         Economist.objects.create(**datas)
-
 
         # print(title)
         # print(c)
